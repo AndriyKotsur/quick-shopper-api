97cf87493b597592ead0a755519e38e1a128b6e0
AndriyKotsur <kotsurandriy@gmail.com>
2025-03-23 20:19:21 +0100

    Develop authentication middleware

---
+++ internal/middleware/authentication.go
@@ -0,0 +1,33 @@
+package middleware
+
+import (
+	"net/http"
+	"strings"
+
+	"github.com/AndriyKotsur/quick-shopper-api/internal/utility/token"
+	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+)
+
+func AuthMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		authHeader := r.Header.Get("Authorization")
+		if authHeader == "" {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		_, err := token.ValidateToken(tokenString, viper.GetString("access_token.public_key"))
+		if err != nil {
+			log.Error().Msgf("Error validating token: %v", err)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		r = r.WithContext(r.Context())
+
+		next.ServeHTTP(w, r)
+	})
+}
