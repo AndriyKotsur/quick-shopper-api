f08af521be660a6f65ff3dfdbbb858ae31328bc9
AndriyKotsur <kotsurandriy@gmail.com>
2025-05-25 16:13:13 +0200

    Update authentication middleware

--- internal/middleware/authentication.go
+++ internal/middleware/authentication.go
@@ -1,15 +1,20 @@
 package middleware
 
 import (
+	"context"
+	"fmt"
 	"net/http"
 	"strings"
 
 	"github.com/AndriyKotsur/quick-shopper-api/internal/utility/token"
+	"github.com/google/uuid"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
-func AuthMiddleware(next http.Handler) http.Handler {
+const userKey = "userId"
+
+func AuthenticateUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		authHeader := r.Header.Get("Authorization")
 
@@ -20,14 +25,15 @@ func AuthMiddleware(next http.Handler) http.Handler {
 
 		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
 
-		_, err := token.ValidateToken(tokenString, viper.GetString("access_token.public_key"))
+		userClaims, err := token.ValidateToken(tokenString, viper.GetString("access_token.public_key"))
 		if err != nil {
 			log.Error().Msgf("Error validating token: %v", err)
 			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
 
-		r = r.WithContext(r.Context())
+		ctx := context.WithValue(r.Context(), userKey, uuid.MustParse(fmt.Sprint(userClaims)))
+		r = r.WithContext(ctx)
 
 		next.ServeHTTP(w, r)
 	})
